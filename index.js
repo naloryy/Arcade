e364385a1ea982f830deb6da1a7d5419c00aacc2
Nalory01 <Panalory@gmail.com>
2023-03-06 13:05:28 -0600

    edit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const playerX = 'x';
+const playerO = 'o';
+const cellBoard = document.querySelector('.cellBoard');
+const nextPlayer = document.querySelector('.nextPlayer');
+const outcome = document.querySelector('.outcome');
+
+
+
+ 
+                
+
+        
+
+
+
+
+
+
+  
+
+
+ 
+ 
